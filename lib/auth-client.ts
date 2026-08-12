0bebf41a2ebd18d238ec133e41a48267e41c73a0
Small Wu <bada4564565@gmail.com>
2026-08-12 21:26:15 +0800

    fix auth session on pdf page

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,11 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL:
-    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
-    (typeof window !== "undefined"
+    typeof window !== "undefined"
       ? window.location.origin
-      : undefined),
+      : process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+
+  fetchOptions: {
+    credentials: "include",
+  },
 });
